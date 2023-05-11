286f9c317792971c12971bdd4753010c8b326b16
Sidney Miranda <41812064+SidMiranda@users.noreply.github.com>
2023-05-11 12:04:33 -0300

    first commit

---
+++ config_instalador.php
@@ -0,0 +1,29 @@
+<?php
+
+define('MAIL_SERVIDOR', 'smtp.diferencialandaimes.com.br');
+define('MAIL_USUARIO', 'site=diferencialandaimes.com.br');
+define('MAIL_EMAIL', 'site@diferencialandaimes.com.br');
+define('MAIL_SENHA', 'dif181204#');
+define('MAIL_PORTA', '587');
+define('MAIL_CRIPT', '');
+define('MAIL_IMAP', 'mboxc.f1.ultramail.com.br');
+	
+define('DB_SERVIDOR', 'mysql.diferencialandaimes.com.br');
+define('DB_USUARIO', 'diferencialanda1');
+define('DB_NOME', 'diferencialandaimes1');
+define('DB_SENHA', 'dif181204#');
+define('PREFIXO', 'wp');
+
+define('FTP_SERVIDOR', 'ftp.diferencialandaimes.com.br');
+define('FTP_USUARIO', 'diferencialandai');
+define('FTP_SENHA', 'dif181204#');
+
+define('DOMINIO', 'www.diferencialandaimes.com.br');
+define('DIRETORIO', 'www/loja/');
+define('DIR', 'loja');
+define('SSL', "false");
+define('SITENOME', '');
+define('KUNDE', 'true');
+define('APP_SENHA', 'dif181204#');
+		
+?>
---
+++ wp-content/themes/astra-child/functions/my-functions.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ *  Corrige o acesso ao microthemer pelo link do menu no dashboard do wordpress.
+ *  Ref.: https://wordpress.stackexchange.com/questions/131199/change-url-of-plugin-admin-menu
+ *  Adicionado por Thiago Santos em 08-05-2020
+ */
+add_action( 'admin_menu', 'my_admin_menu', 100 );
+function my_admin_menu()
+{
+    global $menu, $submenu;
+    $parent = 'tvr-microthemer.php';
+    if( !isset($submenu[$parent]) )
+        return;
+        
+        foreach( $submenu[$parent] as $k => $d ){
+            if( $d['2'] == 'tvr-microthemer.php' )
+            {
+                $submenu[$parent][$k]['2'] = 'admin.php?page=tvr-microthemer.php&mt_preview_url='.get_site_url().'/&_wpnonce='.wp_create_nonce( 'mt-preview-nonce' ).'';
+                break;
+            }
+        }
+}
+
+/*
+ *  Corrige a customização do tema quando não existe widgets.
+ *  Ref.: https://core.trac.wordpress.org/ticket/27965
+ *  Adicionado por Thiago Santos em 08-05-2020
+ */
+add_filter('sidebars_widgets', 'my_sidebars_widgets', 999);
+
+function my_sidebars_widgets( $array ) {
+    if ( !is_array( $array ) ) {
+        $array = array();
+    }
+    return $array;
+}
